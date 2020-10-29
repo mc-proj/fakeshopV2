f5c44f25586f9446ce546a79f0a1c7c416d007dd
mcprojdev <cpl.skratchov@gmail.com>
2020-10-29 17:31:23 +0100

    continuation design header + footer

--- public/js/base.script.js
+++ public/js/base.script.js
@@ -19,4 +19,49 @@ $(document).ready(function() {
 
         $(this).find('[data-toggle=dropdown]').dropdown('toggle');
     })
+
+    $("#bouton-fermeture-lateral").on("click", function() {
+
+        if($("#menu-lateral").is(":visible")) {
+
+            $("#cache").css("display", "none");
+        }
+
+        else {
+
+            $("#cache").css("display", "block");
+        }
+
+        $("#menu-lateral").animate({'width': 'toggle'}, 300);
+    });
+
+    $("#burger-button").on("click", function() {
+
+        $("#bouton-fermeture-lateral").trigger("click");
+    })
+
+    ///--------------
+
+    // $("#burger-button").on("shown.bs.collapse", function() {
+
+    //     alert("ok");
+    // })
+
+    // $("#navbarNav").on("toggle", function() {
+
+    //     console.log("ok");
+    // })
+
+    $(window).resize(function(){
+        
+        if($("#burger-button").is(":visible")) {
+
+            console.log("burger visible");
+        }
+
+        else {
+
+            console.log("burger invisible");
+        }
+    })
 })
